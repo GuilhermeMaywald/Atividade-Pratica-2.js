ec5fba4c77ccbb82d3b69eab4c1801268c8b7a2b
guilherme maywald <guilhermemaywald@gmail.com>
2022-08-25 08:35:34 -0300

    Atividade Prática 2 JS

---
+++ js/aula2.js
@@ -0,0 +1,20 @@
+{ // Resposta letra a:
+    let pacote = 7;
+    let conteudoPacote = 30;
+    let naoColados = 15;
+    let resultado = 0;
+
+    resultado = pacote * conteudoPacote - naoColados;
+    console.log('Márcia colou no álbum ' + resultado + ' adesivos.');
+}
+
+{ // Resposta letra b:
+    let caixaRefri = 24;
+    let totalRefri = caixaRefri * 4;
+    console.log('Cabem ' + totalRefri + ' refrigerantes em 4 caixas.');
+}
+
+{ // Resposta letra c:
+    let decremento = 10;
+    console.log(decremento--);
+}
